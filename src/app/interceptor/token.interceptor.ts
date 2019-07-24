08c6a74c965857828064eb60dc4b3bef70b9508c
80characters <80chars@gmail.com>
2019-07-24 17:10:10 +0700

    refactor

--- src/app/interceptor/token.interceptor.ts
+++ src/app/interceptor/token.interceptor.ts
@@ -5,7 +5,7 @@ import {
     HttpEvent,
     HttpInterceptor
 } from '@angular/common/http';
-import { Observable, from } from 'rxjs';
+import { Observable } from 'rxjs';
 import { AuthService } from '../services/auth/auth.service';
 
 @Injectable()
